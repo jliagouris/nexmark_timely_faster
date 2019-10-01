dec1788d3d2046be36a9639c7143ebecbc102db9
vasia <vasiliki.kalavri@inf.ethz.ch>
2019-10-01 20:39:44 +0200

    convert int key into big endian format to work with rocksdb default comparator

--- src/queries/window_1_rocksdb.rs
+++ src/queries/window_1_rocksdb.rs
@@ -30,7 +30,7 @@ pub fn window_1_rocksdb<S: Scope<Timestamp = usize>>(
             "Accumulate records",
             None,
             move |input, output, notificator, state_handle| {
-                let mut window_contents = state_handle.get_managed_map("window_contents");
+                let mut window_contents = state_handle.get_managed_map(&"");
                 let prefix_key_len: usize = window_contents.as_ref().get_key_prefix_length();
                 let mut buffer = Vec::new();
 
@@ -43,10 +43,10 @@ pub fn window_1_rocksdb<S: Scope<Timestamp = usize>>(
 
                     // Add window margins
                     if last_slide != slide {
-                        println!("Inserting dummy record:: time: {:?}, value:{:?}", slide - window_slide_ns, 0);
-                        window_contents.insert(slide - window_slide_ns, 0);  // Start timestamp of window
-                        println!("Inserting dummy record:: time: {:?}, value:{:?}", window_end, 0);
-                        window_contents.insert(window_end, 0);  // End timestamp of window
+                        //println!("Inserting dummy record:: time: {:?}, value:{:?}", slide - window_slide_ns, 0);
+                        window_contents.insert((slide - window_slide_ns).to_be(), 0);  // Start timestamp of window
+                        //println!("Inserting dummy record:: time: {:?}, value:{:?}", window_end, 0);
+                        window_contents.insert(window_end.to_be(), 0);  // End timestamp of window
                         last_slide = slide;
                     }
                     // Add window contents
@@ -55,8 +55,8 @@ pub fn window_1_rocksdb<S: Scope<Timestamp = usize>>(
                         let key = record.1; // Event time
                         let auction_id = record.0;
                         // Add record 
-                        println!("Inserting window record:: time: {}, value:{}", key, auction_id);
-                        window_contents.insert(key, auction_id);
+                        //println!("Inserting window record:: time: {}, value:{}", key, auction_id);
+                        window_contents.insert(key.to_be(), auction_id);
                     }
                 });
 
@@ -64,28 +64,29 @@ pub fn window_1_rocksdb<S: Scope<Timestamp = usize>>(
                     let window_end = cap.time(); 
                     let window_start = window_end - (window_slide_ns * window_slice_count);  
                     let first_slide_end = window_start + window_slide_ns; // To know which records to delete
-                    println!("Start of window: {}", window_start);
-                    println!("End of window: {}", *window_end);
-                    println!("End of first slide: {}", first_slide_end);
+                    //println!("Start of window: {}", window_start);
+                    //println!("End of window: {}", *window_end);
+                    //println!("End of first slide: {}", first_slide_end);
                     let mut to_delete = Vec::new();  // Keep keys to delete here
                     to_delete.push(window_start);
                     {
-                        let mut window_iter = window_contents.iter(window_start);
+                        let mut window_iter = window_contents.iter(window_start.to_be());
                         let _ = window_iter.next();  // Skip dummy record
                         for (ser_key, ser_value) in window_iter {
                             let k = &ser_key[prefix_key_len..];  // Ignore prefix
-                            let timestamp: usize = bincode::deserialize(unsafe {
+                            let mut timestamp: usize = bincode::deserialize(unsafe {
                                                         std::slice::from_raw_parts(k.as_ptr(), k.len())
                                                     }).expect("Cannot deserialize timestamp");
+                            timestamp = usize::from_be(timestamp);
                             let auction_id: usize = bincode::deserialize(unsafe {
                                                         std::slice::from_raw_parts(ser_value.as_ptr(), ser_value.len())
                                                     }).expect("Cannot deserialize auction id");
-                            println!("Found record:: time: {}, value:{}", timestamp, auction_id);
+                            //println!("Found record:: time: {}, value:{}", timestamp, auction_id);
                             if timestamp == *window_end {  // Omit dummy record
                                 break;
                             }
-                            //assert!(timestamp < *window_end);
-                            println!("Output record:: time: {}, value:{}", timestamp, auction_id);
+                            assert!(timestamp < *window_end);
+                            //println!("Output record:: time: {}, value:{}", timestamp, auction_id);
                             output.session(&cap).give((timestamp, auction_id));
                             if timestamp < first_slide_end {
                                 to_delete.push(timestamp);
@@ -94,8 +95,8 @@ pub fn window_1_rocksdb<S: Scope<Timestamp = usize>>(
                     }
                     // Purge state of first slide in window
                     for ts in to_delete {
-                        println!("Removing record:: time: {}", ts);
-                        window_contents.remove(&ts).expect("Record to remove must exist");
+                        //println!("Removing record:: time: {}", ts);
+                        window_contents.remove(&ts.to_be()).expect("Record to remove must exist");
                     }
                 });
             },
